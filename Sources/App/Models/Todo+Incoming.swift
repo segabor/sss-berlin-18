c0f5d959df84c88fa9d28c28a7215a1df9ce89e1
Gábor Sebestyén <segabor@gmail.com>
2018-09-12 10:38:18 +0200

    first commit

---
+++ Sources/App/Models/Todo+Incoming.swift
@@ -0,0 +1,19 @@
+import Vapor
+
+extension Todo {
+    struct Incoming: Content {
+        var title: String?
+        var completed: Bool?
+        var order: Int?
+
+        func makeTodo() -> Todo {
+            return Todo(
+                id: nil,
+                title: title ?? "",
+                completed: completed ?? false,
+                order: order
+            )
+        }
+    }
+}
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,31 @@
 import Vapor
 
 /// Register your application's routes here.
-public func routes(_ router: Router) throws {
+public func routes(_ todos: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    todos.get("hello") { req in
         return "Hello, world!"
+    }
+
+    struct JSONExample: Content {
+        let name: String
+        let age: Int
+        let birthday: Date
+    }
+
+    todos.get("json") { req -> JSONExample in
+        return JSONExample(name: "Hello", age: 28, birthday: Date())
     }
 
     // Example of configuring a controller
     let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    let todos = todos.grouped("todos")
+    let todo = todos.grouped(Todo.parameter)
+
+    todos.get(use: todoController.index)
+    todo.get(use: todoController.view)
+    todos.post(use: todoController.create)
+    todo.patch(use: todoController.update)
+    todo.delete(use: todoController.delete)
+    todos.delete(use: todoController.clear)
 }
